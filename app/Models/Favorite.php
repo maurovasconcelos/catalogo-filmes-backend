22e75a7879bbd8d98f9805134e056e21f3cd9fed
Mauro Vasconcelos <maurohunt1@gmail.com>
2025-06-01 14:10:09 -0300

    feat: cria modelo e migração para favoritos

---
+++ app/Models/Favorite.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Favorite extends Model
+{
+    use HasFactory;
+
+    /**
+     * Os atributos que são atribuíveis em massa.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'tmdb_id',
+        'title',
+        'poster_path',
+        'overview',
+        'release_date',
+        'vote_average',
+        'user_id',
+    ];
+
+    /**
+     * Os atributos que devem ser convertidos.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'vote_average' => 'float',
+    ];
+}
