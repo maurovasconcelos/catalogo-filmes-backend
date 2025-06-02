f25290e51d6812b2551ee197d2e12a043a594658
Mauro Vasconcelos <maurohunt1@gmail.com>
2025-06-02 15:36:21 -0300

    Adiciona configuração base do Swagger no Controller principal

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,34 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+/**
+ * @OA\Info(
+ *     title="API de Catálogo de Filmes",
+ *     version="1.0.0",
+ *     description="API para gerenciamento de filmes favoritos",
+ *     @OA\Contact(
+ *         email="contato@exemplo.com",
+ *         name="Suporte API"
+ *     ),
+ *     @OA\License(
+ *         name="MIT",
+ *         url="https://opensource.org/licenses/MIT"
+ *     )
+ * )
+ * @OA\Server(
+ *     url="/api",
+ *     description="API Server"
+ * )
+ * @OA\Tag(
+ *     name="Favoritos",
+ *     description="Operações relacionadas aos filmes favoritos"
+ * )
+ */
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
 }
